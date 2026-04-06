c7c46a2075331a779d8d11aee4dbdd16a8839462
PabloCavillon <pabloa.cavillon@gmail.com>
2026-04-06 00:44:59 -0300

    build base store pages

---
+++ src/app/store/orders/[code]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderStorePage() {
+  return (
+    <div>
+      <h1>Order Page</h1>
+    </div>
+  );
+}
---
+++ src/app/store/orders/page.tsx
@@ -0,0 +1,8 @@
+
+export default function OrdersStorePage() {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+    </div>
+  );
+}
